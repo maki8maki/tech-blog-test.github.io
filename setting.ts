542245c44f5b0137284080ea4e5a24ef87b6446f
yuki_y <chrisrerei125815@gmail.com>
2024-01-18 19:12:38 +0900

    update setting.ts

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://";
+export const TARGET_URL = "https://maki8maki.github.io/tech-blog-test.github.io/";
